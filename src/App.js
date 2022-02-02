9b664760617c7708a2ee06d8e1807c9c3dbedd18
hyeongirlife <leehyeongul@gmail.com>
2022-02-02 15:51:43 +0900

    material-ui에서 tables를 사용해 ui를 구현해봄

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles'
+
+// 해당 파일에서 필요한 컴포넌트만 가져올 때 중괄호로 감쌈
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -37,16 +57,31 @@ const customers = [
 ]
 
 
-function App() {
+function App(props) {
+  const { classes } = props
   return (
-    <div>
-      {customers.map((info) => {
-        return <Customer key={info.id} id={info.id} image={info.image} name={info.name} birthday={info.birthday} gender={info.gender} job={info.job}
-        />
-      })}
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>{/* 내용을 출력하는 부분 */}
+          {customers.map((info) => {
+            return <Customer key={info.id} id={info.id} image={info.image} name={info.name} birthday={info.birthday} gender={info.gender} job={info.job}
+            />
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
     //div태그로 꼭 감싸줘야함!!
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
